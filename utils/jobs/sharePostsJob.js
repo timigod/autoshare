930e14128b8d39ddcb41148d7fe310a562d7c34d
Timi Ajiboye <timi@helloworld.ng>
2019-04-11 03:35:59 +0100

    await share in job

--- utils/jobs/sharePostsJob.js
+++ utils/jobs/sharePostsJob.js
@@ -4,7 +4,7 @@ const Post = require('../../models/post')
 module.exports = {
   key: 'share posts',
   jobFunction: async (job, done) => {
-    share(Post.randomShareable)
+    await share(Post.randomShareable)
     done()
   }
 }
